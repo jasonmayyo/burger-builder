b516d79da74f69dd752682d62ca62b577d1c5049
jasonmayyo <jason.m1122332@gmail.com>
2021-03-09 19:36:07 +0200

    Added Build Controls Css

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,8 +7,9 @@ class burgerBuilder extends Component {
     state = {
         ingredients: {
             salad: 1,
-            meat: 3,
-            cheese: 2,
+            meat: 1,
+            cheese: 1,
+            bacon: 1,
         }
     }
     render () {
